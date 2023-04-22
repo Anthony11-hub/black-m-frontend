82ad8998631e45c2ee159603191d3d1fcbe2aed8
Anthony11-hub <mollycruz966@gmail.com>
2023-04-23 02:03:44 +0300

    media query update upto 411px

--- script.js
+++ script.js
@@ -17,26 +17,27 @@ if (close) {
 
 // js for items row
 // Get the container element and the items inside it
-// const container = document.querySelector('.pro-container');
-// const items = container.querySelectorAll('.pro');
+const container = document.querySelector('.pro-container');
+const items = container.querySelectorAll('.pro');
 
-// const containerWidth = container.offsetWidth;
-// const itemWidth = items[0].offsetWidth + parseFloat(getComputedStyle(items[0]).marginLeft) + parseFloat(getComputedStyle(items[0]).marginRight);
-// const itemsPerRow = Math.floor(containerWidth / itemWidth);
+const containerWidth = container.offsetWidth;
+const itemWidth = items.offsetWidth + parseFloat(getComputedStyle(items[0]).marginLeft) + parseFloat(getComputedStyle(items[0]).marginRight);
+const itemsPerRow = Math.floor(containerWidth / itemWidth);
 
-// for (let i = 0; i < items.length; i += itemsPerRow) {
-//   const rowItems = Array.from(items).slice(i, i + itemsPerRow);
-//   if (rowItems.length < itemsPerRow) {
-//     const empty = document.createElement('div');
-//     empty.classList.add('empty');
-//     rowItems[rowItems.length - 1].after(empty);
-//   }
-// }
+for (let i = 0; i < items.length; i += itemsPerRow) {
+  const rowItems = Array.from(items).slice(i, i + itemsPerRow);
+  if (rowItems.length < itemsPerRow) {
+    const empty = document.createElement('div');
+    empty.classList.add('empty');
+    rowItems[rowItems.length - 1].after(empty);
+  }
+}
 
 // ===================================modified
 // Get the container element and the items inside it
-// const container = document.querySelector('.pro-container');
-// const items = container.querySelectorAll('.pro');
+// Get the container element and the items inside it
+// const container = document.querySelector('.container');
+// const items = container.querySelectorAll('.item');
 
 // const containerWidth = container.offsetWidth;
 // const itemWidth = items[0].offsetWidth + parseFloat(getComputedStyle(items[0]).marginLeft) + parseFloat(getComputedStyle(items[0]).marginRight);
@@ -57,32 +58,40 @@ if (close) {
 
 
 
-// Get the container element and the items inside it
-const container = document.querySelector('.pro-container');
-const items = container.querySelectorAll('.pro');
 
-// Calculate the number of items per row based on the container width and the item width
-const containerWidth = container.offsetWidth;
-const itemWidth = items[0].offsetWidth + parseFloat(getComputedStyle(items[0]).marginLeft) + parseFloat(getComputedStyle(items[0]).marginRight);
-const itemsPerRow = Math.floor(containerWidth / itemWidth);
+// const container = document.querySelector('.pro-container');
+// const items = container.querySelectorAll('.pro');
 
-// Check the screen width and adjust the number of empty elements accordingly
-let emptyCount = 3; // default value
-if (itemsPerRow === 2 && window.innerWidth < 768) {
-  emptyCount = 1;
+
+// const containerWidth = container.offsetWidth;
+// const itemWidth = items[0].offsetWidth + parseFloat(getComputedStyle(items[0]).marginLeft) + parseFloat(getComputedStyle(items[0]).marginRight);
+// const itemsPerRow = Math.floor(containerWidth / itemWidth);
+
+
+// let emptyCount = 3; // default value
+// if (itemsPerRow === 2 && window.innerWidth < 768) {
+//   emptyCount = 1;
+// }
+
+
+// for (let i = 0; i < items.length; i += itemsPerRow) {
+//   const rowItems = Array.from(items).slice(i, i + itemsPerRow);
+//   const lastItem = rowItems[rowItems.length - 1];
+//   if (lastItem) {
+//     for (let j = 0; j < emptyCount; j++) {
+//       const empty = document.createElement('div');
+//       empty.classList.add('empty');
+//       lastItem.after(empty);
+//     }
+//   }
+// }
+
+
+// js code for ellipse
+var maxLength = 6;
+var text = $('#ellipse').text();
+if (text.length > maxLength) {
+  text = text.substring(0, maxLength) + '...';
+  $('#ellipse').text(text);
 }
-
-// Loop through the items and add the appropriate number of empty elements after the last item in each row
-for (let i = 0; i < items.length; i += itemsPerRow) {
-  const rowItems = Array.from(items).slice(i, i + itemsPerRow);
-  const lastItem = rowItems[rowItems.length - 1];
-  if (lastItem) {
-    for (let j = 0; j < emptyCount; j++) {
-      const empty = document.createElement('div');
-      empty.classList.add('empty');
-      lastItem.after(empty);
-    }
-  }
-}
-
 
